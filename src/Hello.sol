ecbf1e041f63880f112c86926173aed69389c20a
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2023-12-19 04:26:19 +0100

    remove foundry

--- src/Hello.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
-
-contract Contract {
-  function Hello() public pure returns (string memory) { 
-      return "Hello From Polygon"; 
-  }
-}
